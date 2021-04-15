47dd8642cac96095a877915f7bdc36e4529c0051
ONPA\kds <shiki!3261>
2021-04-15 13:17:44 +0900

    프로젝트 생성

---
+++ src/main/java/com/kito/springwebservice/web/WebRestController.java
@@ -0,0 +1,12 @@
+package com.kito.springwebservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController{
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+}
